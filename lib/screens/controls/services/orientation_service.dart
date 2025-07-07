e939d3b7bc727c048de785c4d9ebccd3b2c0de4c
WangodaFrancis <fwangoda@gmail.com>
2025-07-07 20:48:36 +0300

    Add OrientationService for managing screen orientation modes

---
+++ lib/screens/controls/services/orientation_service.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/services.dart';
+
+class OrientationService {
+  /// Switch to landscape mode for robot control
+  static void switchToLandscapeMode() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+
+  /// Switch to portrait mode for Bluetooth connection
+  static void switchToPortraitMode() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  /// Restore all orientations when leaving the screen
+  static void restoreAllOrientations() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ]);
+  }
+}
